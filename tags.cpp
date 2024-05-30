c0b5b3eec28205ddd9d9c711b33071b80c64d6c6
Tamsanqa <tamsanqathwala62@gmail.com>
2024-05-30 23:33:40 +0200

    added processing of nested tags function

--- tags.cpp
+++ tags.cpp
@@ -6,7 +6,7 @@
 std::vector<tag_struct> all_tags; // vector containing all tag structs
 std::vector<std::string> file_data; // vector to store lines from file
 std::stack<std::string> file_data_stack; // stack containg lines from file 
-std::string current_tag_name; // name of tag currently in processing
+// std::string current_tag_name; // name of tag currently in processing
 std::stack<std::string> tag_names_stack; // name of tags not fully processed
 /**
  * prints all tags and their data contained in the tags vector
@@ -158,40 +158,48 @@ void process_tags(){
      for(int i = 0; i < file_data.size(); ++i){
 
         std::string current_string = file_data[i];
-        process_string(current_string);
-        int start_of_closing_tag = current_string.find("</");
-        int start_of_opening_tag = current_string.find_first_of(">");
+        // process_string(current_string);
+        
+        int closing_tag_start_index = current_string.find("</");
+        int opening_tag_end_index = current_string.find_first_of(">");
 
-        if(start_of_closing_tag < start_of_opening_tag && start_of_closing_tag != -1){
-            // closing tag at the start of text/string
-            std::string tag_name = current_string.substr(1, start_of_opening_tag - start_of_closing_tag - 1);
-            std::cout << "Closing tag name " << tag_name << std::endl;
+        // std::cout << "end op: " << opening_tag_end_index << "; closing start: " << closing_tag_start_index << "; string: " <<  current_string << std::endl;
+
+        if(closing_tag_start_index < opening_tag_end_index && closing_tag_start_index != -1){
+            // opening tag only
             
-            std::cout << "Nested Tags: ";
-            std::cout << current_string << std::endl;
+            std::string tag_name = current_string.substr(2, opening_tag_end_index - 2);
+            tag_names_stack.push(tag_name); // push tag name to stack
+            std::cout << "top of stack " << tag_names_stack.top()<< std::endl;
 
         }
-        else if(start_of_closing_tag > start_of_opening_tag){
+        else if(closing_tag_start_index > opening_tag_end_index){
             // closing tag at the end of the text, therefore one tag type
-            std::cout << "Straigt tags: ";
+            int opening_tag_index = current_string.find_first_of(">");
+            int closing_tag_index = current_string.find_last_of("<");
+            std::string tag_name = get_tag_name(current_string, opening_tag_index);
+            tag_names_stack.push(tag_name);
             process_string(current_string);
-            tag_names_stack.pop();
-            std::cout << current_string << std::endl;
+            if(!tag_names_stack.empty()){
+                tag_names_stack.pop();
+            }
+            // std::cout << current_string << std::endl;
         }
-        else if(start_of_closing_tag == -1 && start_of_opening_tag == -1){
+        else if(closing_tag_start_index == -1 && opening_tag_end_index == -1){
             //tags not found in current text
             std::string tag_name = tag_names_stack.top(); // read tag from stack
             int tag_index = find_tag(tag_name);
             all_tags[tag_index].number_of_pairs += 1;
             all_tags[tag_index].text = all_tags[tag_index].text + ":" + current_string;
 
-            std::cout << "Clean text: " << current_string << std::endl;
+            // std::cout << "Clean text: " << current_string << std::endl;
         }
-        else if(start_of_closing_tag == -1 && start_of_opening_tag != -1){
+        else if(closing_tag_start_index == -1 && opening_tag_end_index != -1){
             // only opening tag in string
             int opening_tag_index = file_data[i].find_first_of(">");
             int closing_tag_index = file_data[i].find_last_of("<");
             std::string new_tag_name = get_tag_name(file_data[i], opening_tag_index);
+            // std::cout << "tag name: " << new_tag_name << std::endl;
             tag_names_stack.push(new_tag_name);
             if(closing_tag_index < file_data[i].size()){
                 int tag_index = find_tag(new_tag_name);
@@ -206,6 +214,7 @@ void process_tags(){
                     new_tag.name = new_tag_name;
                     new_tag.number_of_pairs = 1;
                     new_tag.text = file_data[i].substr(file_data[i].size() - opening_tag_index);
+                    all_tags.push_back(new_tag); // add tag to tags vector
                 }
             }
         }
@@ -213,7 +222,7 @@ void process_tags(){
 }
 
 void print_stack(){
-    std::cout << file_data_stack.top().find("</") << std::endl;
+    std::cout << tag_names_stack.top() << std::endl;
 }
 /*
  * clears the system terminal when called
